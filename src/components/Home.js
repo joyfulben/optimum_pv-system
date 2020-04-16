db8cd69f7470e8321f5506078fc2685049c7d6d2
joyfulben <benfishernow@gmail.com>
2020-04-16 15:15:21 -0600

    added diagram with explanation in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,10 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-      <p>This has cool info</p>
+        <a href="https://www.energy.gov/energysaver/planning-home-solar-electric-system"><img src="./PV_system_diagram.jpeg" alt="" /></a>
+        <div>
+        <h6>The link above brings you to a site that helps you plan a residential, solar-electric system provided by energy.gov. </h6>
+        </div>
       </div>
     )
   }
