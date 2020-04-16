353d1640c80441193a941169a2b234fc66ccc138
joyfulben <benfishernow@gmail.com>
2020-04-16 15:21:31 -0600

    debugging PV_system_diagram.jpeg source

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <a href="https://www.energy.gov/energysaver/planning-home-solar-electric-system"><img src="./PV_system_diagram.jpeg" alt="" /></a>
+        <a href="https://www.energy.gov/energysaver/planning-home-solar-electric-system"><img src="/PV_system_diagram.jpeg" alt="" /></a>
         <div>
-        <h6>The link above brings you to a site that helps you plan a residential, solar-electric system provided by energy.gov. </h6>
+        <h6>The link above brings you to a site that helps you plan a residential, solar-electric system provided by energy.gov </h6>
         </div>
       </div>
     )
